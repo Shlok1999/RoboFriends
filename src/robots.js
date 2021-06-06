5460b3ec4946272d1ecb2cff8d6f9e8ff0abc659
shlok1999 <09shlok1999ae1@gmail.com>
2021-06-06 19:42:50 +0530

    Getting Started with react

---
+++ src/robots.js
@@ -0,0 +1,55 @@
+export const robots=[
+    {
+        id: 1,
+        name: 'Shlok Ranjan1',
+        email: 'Hello@gmail.com'
+
+    },
+    {
+        id: 2,
+        name: 'zeher25',
+        email: 'Hello@gmail.com'
+
+    },
+    {
+        id: 3,
+        name: 'shaakaal',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'ZebraaaaX',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'silleter',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Crowlex',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'creamber',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'zohraa',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'SilverX',
+        email: 'Hello@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'PressyreX',
+        email: 'Hello@gmail.com'
+    }
+    
+]
